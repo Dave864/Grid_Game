04f9bcfc13c6ad894529582b866296489294a907
Dave864 <dmacpherson864@gmail.com>
2016-07-30 17:26:45 -0700

    Worked on the GenerateWorld script
    The GenerateWorld script is able to fully load up the visible world
    sections when the game starts. The player is unable to move.

--- Grid_Game/Assets/Scripts/WorldGenerator.cs
+++ Grid_Game/Assets/Scripts/WorldGenerator.cs
@@ -38,24 +38,167 @@ public class WorldGenerator : MonoBehaviour {
 	void LoadWorld () {
 		//float origin_offset = PlayerMovement.Sec_Width;
 		LoadSection (Cur_Sec, SECTION_LOC.CUR);
-
+		LoadSection (Cur_Sec - 1, SECTION_LOC.L);
+		LoadSection (Cur_Sec + 1, SECTION_LOC.R);
+		LoadSection (Cur_Sec - 10, SECTION_LOC.T);
+		LoadSection (Cur_Sec + 10, SECTION_LOC.B);
+		LoadSection (Cur_Sec - 11, SECTION_LOC.TL);
+		LoadSection (Cur_Sec - 9, SECTION_LOC.TR);
+		LoadSection (Cur_Sec + 9, SECTION_LOC.BL);
+		LoadSection (Cur_Sec + 11, SECTION_LOC.BR);
 	}
 
-	// Loads section sec_num of the world at location at coordinates: sec_origin_x, some height, sec_origin_z
+	// Loads section sec_num of the world at location if sec_num is valid
 	void LoadSection (int sec_num, SECTION_LOC location) {
+		if (sec_num < 0 || !Directory.Exists (GetSectionPath (sec_num))) {
+			return;
+		}
 		Sec_Data = (TextAsset)Resources.Load (GetAreaMapPath (sec_num), typeof(TextAsset));
 		int c_col = (int)Cur_Sec_Origin_x;
 		int c_row = (int)Cur_Sec_Origin_z;
+		int origin_offset = (int)PlayerMovement.Sec_Width;
 		float height = 0.0f;
 		string[] row_data = Sec_Data.text.Split ('\n');
 		if (location == SECTION_LOC.L) {
+			if (Left.childCount == 0) {
+				c_col -= origin_offset;
+				SectionData.L_Sec = new string[row_data.Length][];
+				for (int i = 0; i < row_data.Length; i++) {
+					SectionData.L_Sec [i] = row_data [i].Split (' ');
+				}
+				foreach (string[] row in SectionData.L_Sec) {
+					foreach (string cell in row) {
+						height = (float)(char.GetNumericValue (cell [3]) / 2);
+						MakeCell (c_row, c_col, height, cell, Left);
+						c_col += 1;
+					}
+					c_row -= 1;
+					c_col = (int)Cur_Sec_Origin_x - origin_offset;
+				}
+			}
 		} else if (location == SECTION_LOC.R) {
+			if (Right.childCount == 0) {
+				c_col += origin_offset;
+				SectionData.R_Sec = new string[row_data.Length][];
+				for (int i = 0; i < row_data.Length; i++) {
+					SectionData.R_Sec [i] = row_data [i].Split (' ');
+				}
+				foreach (string[] row in SectionData.R_Sec) {
+					foreach (string cell in row) {
+						height = (float)(char.GetNumericValue (cell [3]) / 2);
+						MakeCell (c_row, c_col, height, cell, Right);
+						c_col += 1;
+					}
+					c_row -= 1;
+					c_col = (int)Cur_Sec_Origin_x + origin_offset;
+				}
+			}
 		} else if (location == SECTION_LOC.T) {
+			if (Top.childCount == 0) {
+				c_row += origin_offset;
+				SectionData.T_Sec = new string[row_data.Length][];
+				for (int i = 0; i < row_data.Length; i++) {
+					SectionData.T_Sec [i] = row_data [i].Split (' ');
+				}
+				foreach (string[] row in SectionData.T_Sec) {
+					foreach (string cell in row) {
+						height = (float)(char.GetNumericValue (cell [3]) / 2);
+						MakeCell (c_row, c_col, height, cell, Top);
+						c_col += 1;
+					}
+					c_row -= 1;
+					c_col = (int)Cur_Sec_Origin_x;
+				}
+			}
 		} else if (location == SECTION_LOC.B) {
+			if (Bot.childCount == 0) {
+				c_row -= origin_offset;
+				SectionData.B_Sec = new string[row_data.Length][];
+				for (int i = 0; i < row_data.Length; i++) {
+					SectionData.B_Sec [i] = row_data [i].Split (' ');
+				}
+				foreach (string[] row in SectionData.B_Sec) {
+					foreach (string cell in row) {
+						height = (float)(char.GetNumericValue (cell [3]) / 2);
+						MakeCell (c_row, c_col, height, cell, Bot);
+						c_col += 1;
+					}
+					c_row -= 1;
+					c_col = (int)Cur_Sec_Origin_x;
+				}
+			}
 		} else if (location == SECTION_LOC.TL) {
+			if (Top_L.childCount == 0) {
+				c_col -= origin_offset;
+				c_row += origin_offset;
+				SectionData.TL_Sec = new string[row_data.Length][];
+				for (int i = 0; i < row_data.Length; i++) {
+					SectionData.TL_Sec [i] = row_data [i].Split (' ');
+				}
+				foreach (string[] row in SectionData.TL_Sec) {
+					foreach (string cell in row) {
+						height = (float)(char.GetNumericValue (cell [3]) / 2);
+						MakeCell (c_row, c_col, height, cell, Top_L);
+						c_col += 1;
+					}
+					c_row -= 1;
+					c_col = (int)Cur_Sec_Origin_x - origin_offset;
+				}
+			}
 		} else if (location == SECTION_LOC.TR) {
+			if (Top_R.childCount == 0) {
+				c_col += origin_offset;
+				c_row += origin_offset;
+				SectionData.TR_Sec = new string[row_data.Length][];
+				for (int i = 0; i < row_data.Length; i++) {
+					SectionData.TR_Sec [i] = row_data [i].Split (' ');
+				}
+				foreach (string[] row in SectionData.TR_Sec) {
+					foreach (string cell in row) {
+						height = (float)(char.GetNumericValue (cell [3]) / 2);
+						MakeCell (c_row, c_col, height, cell, Top_R);
+						c_col += 1;
+					}
+					c_row -= 1;
+					c_col = (int)Cur_Sec_Origin_x + origin_offset;
+				}
+			}
 		} else if (location == SECTION_LOC.BL) {
+			if (Bot_L.childCount == 0) {
+				c_col -= origin_offset;
+				c_row -= origin_offset;
+				SectionData.BL_Sec = new string[row_data.Length][];
+				for (int i = 0; i < row_data.Length; i++) {
+					SectionData.BL_Sec [i] = row_data [i].Split (' ');
+				}
+				foreach (string[] row in SectionData.BL_Sec) {
+					foreach (string cell in row) {
+						height = (float)(char.GetNumericValue (cell [3]) / 2);
+						MakeCell (c_row, c_col, height, cell, Bot_L);
+						c_col += 1;
+					}
+					c_row -= 1;
+					c_col = (int)Cur_Sec_Origin_x - origin_offset;
+				}
+			}
 		} else if (location == SECTION_LOC.BR) {
+			if (Bot_R.childCount == 0) {
+				c_col += origin_offset;
+				c_row -= origin_offset;
+				SectionData.BR_Sec = new string[row_data.Length][];
+				for (int i = 0; i < row_data.Length; i++) {
+					SectionData.BR_Sec [i] = row_data [i].Split (' ');
+				}
+				foreach (string[] row in SectionData.BR_Sec) {
+					foreach (string cell in row) {
+						height = (float)(char.GetNumericValue (cell [3]) / 2);
+						MakeCell (c_row, c_col, height, cell, Bot_R);
+						c_col += 1;
+					}
+					c_row -= 1;
+					c_col = (int)Cur_Sec_Origin_x + origin_offset;
+				}
+			}
 		} else {
 			SectionData.Cur_Sec = new string[row_data.Length][];
 			for (int i = 0; i < row_data.Length; i++) {
@@ -63,7 +206,7 @@ public class WorldGenerator : MonoBehaviour {
 			}
 			foreach (string[] row in SectionData.Cur_Sec) {
 				foreach (string cell in row) {
-					height = (float)(char.GetNumericValue (cell[3])/2);
+					height = (float)(char.GetNumericValue (cell [3]) / 2);
 					MakeCell(c_row, c_col, height, cell, Cur);
 					c_col += 1;
 				}
@@ -83,6 +226,18 @@ public class WorldGenerator : MonoBehaviour {
 		c += (char)(sec_num % 10);
 		file_path += c;
 		file_path += "/AreaMap";
+		return file_path;
+	}
+
+	// Create Section file path for sec_num
+	string GetSectionPath (int sec_num){
+		string file_path = "Assets/Resources/Sections/";
+		char c = '0';
+		c += (char)(sec_num / 10);
+		file_path += c;
+		c = '0';
+		c += (char)(sec_num % 10);
+		file_path += c;
 		return file_path;
 	}
 
