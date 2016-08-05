3243446642292f250af3257d77d5b2bfa89c8ca2
Dave864 <dmacpherson864@gmail.com>
2016-08-05 01:17:16 -0700

    Worked on PlayerMovement script
    The game splits up diagonal movement when horizontal and vertical input
    are recieved at the same time.

--- Grid_Game/Assets/Scripts/PlayerMovement.cs
+++ Grid_Game/Assets/Scripts/PlayerMovement.cs
@@ -25,6 +25,8 @@ public class PlayerMovement : MonoBehaviour {
 	private bool Rotating = false;
 	private bool RotButtDown = false;
 	private Quaternion Rotate_To = Quaternion.Euler(30f, 0f, 0f);
+	enum HELD_DIR_BUTT {VERT, HOZ, NONE};
+	HELD_DIR_BUTT Held_Butt;
 
 	// text showing player coordinates
 	public Text Coordinates_Text;
@@ -63,6 +65,7 @@ public class PlayerMovement : MonoBehaviour {
 		transform.position = new Vector3 (transform.position.x + Cur_Col, transform.position.y + height, transform.position.z - Cur_Row);
 		Debug_Text.text = "Waiting...";
 		DisplayPos ();
+		Held_Butt = HELD_DIR_BUTT.NONE;
 	}
 
 	// Update is called once per frame
@@ -82,6 +85,39 @@ public class PlayerMovement : MonoBehaviour {
 			+ "\nsection: " + Cur_Sec.ToString ()
 			+ "\nrow: " + Cur_Row.ToString ()
 			+ "\ncol: " + Cur_Col.ToString ();
+	}
+
+	// Determines if a directional input is held down
+	void HeldDirButton (float hoz_inc, float vert_inc) {
+		if (Held_Butt == HELD_DIR_BUTT.HOZ) {
+			if (hoz_inc != 0 && vert_inc == 0) {
+				Held_Butt = HELD_DIR_BUTT.HOZ;
+			} else if (hoz_inc == 0 && vert_inc != 0) {
+				Held_Butt = HELD_DIR_BUTT.VERT;
+			} else if (hoz_inc != 0 && vert_inc != 0) {
+				Held_Butt = HELD_DIR_BUTT.HOZ;
+			} else {
+				Held_Butt = HELD_DIR_BUTT.NONE;
+			}
+		} else if (Held_Butt == HELD_DIR_BUTT.VERT) {
+			if (hoz_inc != 0 && vert_inc == 0) {
+				Held_Butt = HELD_DIR_BUTT.HOZ;
+			} else if (hoz_inc == 0 && vert_inc != 0) {
+				Held_Butt = HELD_DIR_BUTT.VERT;
+			} else if (hoz_inc != 0 && vert_inc != 0) {
+				Held_Butt = HELD_DIR_BUTT.VERT;
+			} else {
+				Held_Butt = HELD_DIR_BUTT.NONE;
+			}
+		} else {
+			if (hoz_inc != 0 && vert_inc == 0) {
+				Held_Butt = HELD_DIR_BUTT.HOZ;
+			} else if (hoz_inc == 0 && vert_inc != 0) {
+				Held_Butt = HELD_DIR_BUTT.VERT;
+			} else if (hoz_inc != 0 && vert_inc != 0) {
+				Held_Butt = HELD_DIR_BUTT.HOZ;
+			}
+		}
 	}
 
 	// Keeps player from rotating when rotate button is held down
@@ -120,21 +156,40 @@ public class PlayerMovement : MonoBehaviour {
 	// Adjust input based on player rotation
 	void OrientAxes (float hoz_inc, float vert_inc) {
 		if (Rotation == 0) {
-			PrevDiagMove (hoz_inc, vert_inc);
+			StartCoroutine (PrevDiagMove (hoz_inc, vert_inc));
 		} else if (Rotation == 90 || Rotation == -270) {
-			PrevDiagMove (vert_inc, -hoz_inc);
+			StartCoroutine (PrevDiagMove (vert_inc, -hoz_inc));
 		} else if (Rotation == 180 || Rotation == -180) {
-			PrevDiagMove (-hoz_inc, -vert_inc);
+			StartCoroutine (PrevDiagMove (-hoz_inc, -vert_inc));
 		} else {
-			PrevDiagMove (-vert_inc, hoz_inc);
+			StartCoroutine (PrevDiagMove (-vert_inc, hoz_inc));
 		}
 	}
 
 	// Prevent the player from moving diagonally
-	void PrevDiagMove (float hoz_inc, float vert_inc) {
+	IEnumerator PrevDiagMove (float hoz_inc, float vert_inc) {
 		Debug_Text.text = "Horizontal Input: " + hoz_inc.ToString () + "\nVertical Input: " + vert_inc.ToString ()
 		+ "\nCoordinates: " + transform.position.x + ", " + transform.position.y + ", " + transform.position.z;
-		Move (hoz_inc, vert_inc);
+		// Determine if an input button is being held down
+		HeldDirButton (hoz_inc, vert_inc);
+		// Split horizontal and vertical movement
+		Moving = true;
+		if (hoz_inc != 0 && vert_inc != 0) {
+			if (Held_Butt == HELD_DIR_BUTT.HOZ) {
+				yield return StartCoroutine (Move (hoz_inc, 0.0f));
+				yield return StartCoroutine (Move (0.0f, vert_inc));
+			} else if (Held_Butt == HELD_DIR_BUTT.VERT) {
+				yield return StartCoroutine (Move (0.0f, vert_inc));
+				yield return StartCoroutine (Move (hoz_inc, 0.0f));
+			} else {
+				yield return StartCoroutine (Move (hoz_inc, 0.0f));
+				yield return StartCoroutine (Move (0.0f, vert_inc));
+			}
+		} else {
+			yield return StartCoroutine (Move (hoz_inc, vert_inc));
+		}
+		Moving = false;
+		yield return 0;
 	}
 
 	// Gets the cell type of the destination
@@ -192,7 +247,7 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	// Move the player to destTransform
-	void Move (float hoz_inc, float vert_inc) {
+	IEnumerator Move (float hoz_inc, float vert_inc) {
 		string startCellInfo = SectionData.Cur_Sec [Cur_Row] [Cur_Col];
 		Vector3 destTransform = new Vector3 (transform.position.x + hoz_inc, transform.position.y, transform.position.z + vert_inc);
 		Debug_Text.text = "Horizontal Input: " + hoz_inc.ToString () + "\nVertical Input: " + vert_inc.ToString ()
@@ -204,6 +259,7 @@ public class PlayerMovement : MonoBehaviour {
 		} else {
 			FloorStart (ref destTransform, startCellInfo, (int) hoz_inc, (int) vert_inc);
 		}
+		yield return null;
 	}
 
 	// update section and coordinates
